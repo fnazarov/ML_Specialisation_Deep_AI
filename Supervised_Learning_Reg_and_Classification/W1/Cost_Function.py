3ff19ea903c890d477c35bf74213d85c74d30daf
Farid Nazarov <faridnazarov@gmail.com>
2023-06-21 13:00:58 +0200

    lab utils

--- Supervised_Learning_Reg_and_Classification/W1/Cost_Function.py
+++ Supervised_Learning_Reg_and_Classification/W1/Cost_Function.py
@@ -14,6 +14,42 @@ def compute_cost(x, y, w, b):
 
     return J
 
+def compute_gradient(x, y, w, b):
+    m = x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + m
+        dj_dw_i = (f_wb - y[i]) * x[i]
+        dj_db_i = f_wb - y[i]
+        dj_db += dj_db_i
+        dj_dw += dj_dw_i
+    return dj_dw/m, dj_db/m
+
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient_function):
+
+    J_history = []
+    p_history = []
+    w = w_in
+    b= b_in
+
+    for i in range(num_iters):
+
+        dw, db = gradient_function(x, y, w, b)
+        w = w - alpha * dw
+        b = b - alpha * db
+
+        if i <100000:
+            J_history.append(cost_function(x, y, w, b))
+            p_history.append([w,b])
+
+        #Print cost every at intervals 10 times or as many iterations if i<10
+        if i% math.ceil(num_iters/10) == 0:
+            print(f"Iteration {i:4}: Cost {J_history[-1]}:0.2e} ",
+                  f"dw: {dw:0.3e}, db: {db: 0.3e} ",
+                  f"w:{w: 0.3e}, b: {b:0.5e}")
+    return w, b, J_history, p_history
 
 x_train = np.array([1.0, 1.7, 2.0, 2.5, 3.0, 3.2])
 y_train = np.array([250, 300, 480,  430,   630, 730])
