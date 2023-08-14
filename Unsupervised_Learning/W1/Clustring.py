951621ce7a3fb9a49f86178550224e94d2997b96
Farid Nazarov <faridnazarov@gmail.com>
2023-08-14 10:56:32 +0200

    Clustring

--- Unsupervised_Learning/W1/Clustring.py
+++ Unsupervised_Learning/W1/Clustring.py
@@ -5,13 +5,7 @@ from utils import *
 ### K-means is an iterative procedure that starts by guessing the initial centroiod and then refined this by:
 # repeatedly assigning examples to their closest centroids, and then recomputing the centroid based on the assignments
 
-"""centroids = kMeans_init_centroids(X,K)
 
-for iter in range(iterations:
-    idx = find_closest_centroids(X, centroids)
-
-    centroids = compute_centroids(X,idx, K)
-"""
 def find_closest_centroids(X, centroids):
     """
     Computes the centroids memberships for every example
@@ -25,7 +19,8 @@ def find_closest_centroids(X, centroids):
     idx = np.zeros(m,dtype=int)
 
     for i in range(m):
-        distances = np.sum(X[i] - centroids)**2, axis= 1) #((X[i][0] - centroid[0][0])**2) + ((X[i][1] - centroid[0][1])**2)
+        distances = np.sum((X[i] - centroids)**2, axis= 1) #((X[i][0] - centroid[0][0])**2) + ((X[i][1] - centroid[
+        # 0][1])**2)
         idx[i] = np.argmin(distances)  # Find the index of the centroid with the minimum distance
 
     return idx
@@ -56,3 +51,21 @@ idx = find_closest_centroids(X, initial_centroids)
 K = 3
 centroids = compute_centroids(X, idx, K)
 print(centroids)
+'''
+ ######K-means on a sample dataset¶#######
+
+After completed the two functions (find_closest_centroids and compute_centroids) above, 
+the next step is to run the K-means algorithm on a toy 2D dataset to help  understand how K-means works
+'''
+
+def run_kMeans(X, initial_centroids, max_iters = 10, plot_progress=False):
+    """
+    Runs the K-Means algorithm on data matrix X, where each row of X is a single example
+    :param X: ndarray (m, n) initial data set
+    :param initial_centroids: (K,n) inital centroids for initialization
+    :param max_iters: num of max iterations
+    :param plot_progress: to see the progress
+    :return: centroids, idx
+
+    """
+
