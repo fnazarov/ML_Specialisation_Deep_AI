090b7f35370b9222328817e5f42aafa781212e90
Farid Nazarov <faridnazarov@gmail.com>
2023-08-14 11:01:58 +0200

    Clustring

--- Unsupervised_Learning/W1/Clustring.py
+++ Unsupervised_Learning/W1/Clustring.py
@@ -68,4 +68,38 @@ def run_kMeans(X, initial_centroids, max_iters = 10, plot_progress=False):
     :return: centroids, idx
 
     """
+    m, n = X.shape
+    K = initial_centroids.shape[0]
+    centroids = initial_centroids
+    previous_centroids = centroids
+    idx = np.zeros(m)
+    plt.figure(figsize=(12,7))
 
+    # Run K-Means Algorithms
+
+    for i in range(max_iters):
+
+        #Output Progress
+        print("K-Means iteration %d%d" %(i, max_iters-1))
+
+        #For each example in X, assign it to the closest centroid
+        idx = find_closest_centroids(X, centroids)
+
+        #Optionally plot progess
+        if plot_progress:
+            plot_progress_kMeans(X, centroids, previous_centroids, idx, K, i)
+            previous_centroids = centroids
+
+        centroids = compute_centroids(X, idx, K)
+    plt.show()
+
+    return centroids, idx
+
+# Set initial centroids
+initial_centroids = np.array([[3,3],[6,2],[8,5]])
+
+# Number of iterations
+max_iters = 10
+
+# Run K-Means
+centroids, idx = run_kMeans(X, initial_centroids, max_iters, plot_progress=True)
